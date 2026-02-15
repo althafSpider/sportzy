dc6153f46d920ea0b3f3f2d4007b04fe0fd290ae
althafSpider <muhammed.althaf@spiderworks.in>
2026-02-15 11:56:32 +0530

    Added 24x7

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+import AgentAPI from "apminsight";
+AgentAPI.config();
+
 import express from 'express';
 import matchRoutes from './routes/matches.js';
 import commentaryRoutes from './routes/commentary.js';
